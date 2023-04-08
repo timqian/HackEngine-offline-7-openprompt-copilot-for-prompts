22a55ddf3e2f1c6e4cef4cc40cde4b24b21cb029
timqian <timqian@t9t.io>
2023-04-08 21:47:25 +0800

    update prompt

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -20,7 +20,7 @@ const handler = async (req: Request): Promise<Response> => {
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
     messages: [{ role:"system", content: `
-According to the following guide about how to write better prompt, optimize the prompt I give you. Return only the prompt, no other text
+According to the following guide about how to write better prompt, optimize the prompt I give you. Return the prompt, also tell me what have you optimized
 
     --------------------------------
     ### 2. Put instructions at the beginning of the prompt and use ### or """ to separate the instruction and context 
